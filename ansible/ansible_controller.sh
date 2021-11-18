119dad2680aa8811391d78a7732b18d913052eff
jamesdidit72 <jamesjbarton72@gmail.com>
2021-11-18 14:17:04 +0000

    added ansible commands

---
+++ ansible/ansible_controller.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get install software-properties-common -y
+# line 3 requires the user to press enter to continue
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get upgrade -y
+sudo apt-get install ansible -y
